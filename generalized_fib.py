f552c2875213a824dbf0ea754f15a5e8676c765b
Ryan Fox <ryan@foxrow.com>
2017-03-16 01:13:49 -0500

    nicer printing

--- generalized_fib.py
+++ generalized_fib.py
@@ -13,9 +13,18 @@ def f(n):
     return g
 
 if __name__ == '__main__':
-    order = 5
-    z = f(order)
 
-    print("{}'th order fib sequence".format(order))
-    for i in range(10):
-        print(z(i))
+    functions = 10
+    parameters = 20
+
+    results = []
+
+    for i in range(1, functions + 1):
+        func = f(i)
+        results.append([func(j) for j in range(parameters)])
+
+    width1 = len(str(functions))
+    width2 = len(str(max([max(item) for item in results])))
+    for i, result in enumerate(results):
+            print('{:{width}} '.format(i + 1, width=width1) +
+                  ' '.join('{:{width}}'.format(number, width=width2) for number in result))
